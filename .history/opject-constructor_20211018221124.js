b6155599e4376ed94ee1f9c14b9e8110ce52d021
hieuhulk <hulkgk12082002@gmail.com>
2021-10-19 21:43:15 +0700

    opject constructor

---
+++ .history/opject-constructor_20211018221124.js
@@ -0,0 +1,18 @@
+function User(firstName, lastName, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+
+    this.getName = function () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+var author = new User('Hieu', 'Hulk', 'avatar');
+var user = new User('Konan', 'dev', 'avatar');
+
+author.title = 'Chia se tao FB';
+user.comment = 'Hihi';
+
+console.log(author,getName());
+console.log(user.getName());
---
+++ .history/opject-constructor_20211018221617.js
@@ -0,0 +1,19 @@
+function User(firstName, lastName, age, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.avatar = avatar;
+
+    this.getName = function () {
+        return `${this.firstName} ${this.lastName} ${this.age}`; // tra ve ten
+    }
+}
+
+var author = new User('Hieu', 'Hulk', '19', 'avatar'); // khai bao chuoi author
+var user = new User('Konan', 'dev','20', 'avatar'); // khai bao chuoi user
+
+author.title = 'Chia se tao FB'; // them title vafo chuoi author
+user.comment = 'Hihi'; // them comment vao chuoi user
+
+console.log(author.getName());
+console.log(user.getName());
